7ec23d4f05cebd6071c97f3c2f848e2bc3f9bf90
luccasoli <oliveira.mesquita@aluno.uece.br>
2018-07-04 02:09:18 -0300

    problemas no tab-page

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ClientRoutingModule } from './client-routing.module';
 import { NavBarClientComponent } from './nav-bar-client/nav-bar-client.component';
 import { HomeComponent } from './home/home.component';
+import { AgendamentosComponent } from './agendamentos/agendamentos.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ClientRoutingModule
   ],
-  declarations: [NavBarClientComponent, HomeComponent]
+  declarations: [NavBarClientComponent, HomeComponent, AgendamentosComponent]
 })
 export class ClientModule { }
